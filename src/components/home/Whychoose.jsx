b527c10fc853229c004f8eb790d9fcf9716a0814
rohithkumar4855 <devallakumar4855@gmail.com>
2026-05-25 16:00:17 +0530

    Fix image path case sensitivity for Graph import

--- src/components/home/Whychoose.jsx
+++ src/components/home/Whychoose.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BadgeCheck, Lock, Zap } from "lucide-react";
 import Homescreen from "../../../public/images/homescreen.png";
-import Graph from "../../../public/images/Graphic.png";
+import Graph from "../../../public/images/graphic.png";
 
 const WhyChooseViralPe = () => {
   return (
