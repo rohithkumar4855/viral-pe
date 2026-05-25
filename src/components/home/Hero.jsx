18092862bf15209b92e21fcdee6ae927aaa2fd3a
rohithkumar4855 <devallakumar4855@gmail.com>
2026-05-25 15:58:56 +0530

    Fix case of homescreen image import

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -3,9 +3,7 @@ import Container from "../../layout/Container";
 import Button from "../ui/Button";
 import Appleicon from "../../../public/images/appleicon.png";
 import playstoreicon from "../../../public/images/playstoreicon.png";
-import homescreen from "../../../public/images/Homescreen1.png";
-import Frame_1 from "../../../public/images/Frame_1.png";
-import Frame_2 from "../../../public/images/Frame_2.png";
+import homescreen from "../../../public/images/homescreen1.png";
 import { ArrowLeft, ChevronDown } from "lucide-react";
 
 
